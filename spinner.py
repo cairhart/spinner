f87c1c8ee9be5f393e126c5f926732b481618071
Carter Airhart <airhartcarter@utexas.edu>
2019-08-29 19:10:25 -0700

    added image for testing

--- spinner.py
+++ spinner.py
@@ -1,7 +1,7 @@
 import imageio as io
-reader = io.get_reader()//todo
+reader = io.get_reader('./liliana_spinning.mp4')
 fps = reader.get_meta_data()['fps']
 
-output_frames = io.get_writer('~/liliana_spinning.mp4',fps=fps)
+output_frames = io.get_writer('./liliana_spinning_spun.mp4',fps=fps)
 for framenum, frame in enumerate(reader):
-
+	print(framenum)
